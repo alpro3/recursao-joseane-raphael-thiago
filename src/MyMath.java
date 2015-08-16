eb59a53f8edf683279c2afb17f28c7152e843c2c
Raphael Braun <mbraun.raphael@gmail.com>
2015-08-16 16:03:02 -0300

    Metodos criados em aula

--- src/MyMath.java
+++ src/MyMath.java
@@ -24,45 +24,29 @@ public class MyMath {
     public static int fibonacci(int n) {
         int som = 0;
         int cont = 0;
-        if (n == 0 || n == 1){
+        if (n == 0 || n == 1) {
             som = 1;
-        } else if(cont == n) {
+        } else if (cont == n) {
             return som;
         } else {
-            som = fibonacci(n-1) + fibonacci(n-2);
+            som = fibonacci(n - 1) + fibonacci(n - 2);
             cont++;
         }
         return som;
     }
 
-	public static int somatorioIntervalo(int k, int j){
-		int soma = 0;
-		int count = k;
+    public static int somatorioIntervalo(int k, int j) {
+        int soma = 0;
+        int count = k;
 
-		if((k > j) || (k == j) || (k == j-1)){
-			soma = 0;
-		} else if (k == j - 2){
-			soma = k+1;
-		} else if (k < j-2) {
-			soma = somatorioIntervalo(k,j);
-			count--;
-		}
-		return soma;
-	}
-
-    public static int somatorioIntervalo2(int k, int j) {
-        int som;
-        if(k > j) {
-            som = j;
-        } else {
-            som = k;
+        if ((k > j) || (k == j) || (k == j - 1)) {
+            soma = 0;
+        } else if (k == j - 2) {
+            soma = k + 1;
+        } else if (k < j - 2) {
+            soma = somatorioIntervalo(k, j);
+            count--;
         }
-
-        if (n > 0) {
-            som = n + somatorio(n - 1);
-        } else {
-            som = n + somatorio(n + 1);
-        }
-        return som;
+        return soma;
     }
 }
